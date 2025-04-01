bb657aa2c0739819199ce5629ad89c9ded710fad
Söùrãv <souravpramanick1997@gmail.com>
2025-04-02 04:29:13 +0530

    cronjob to delete opt older than 4hours

---
+++ server/cronJob.js
@@ -0,0 +1,17 @@
+const cron = require("node-cron");
+const OTPModel = require("./models/otp.model");
+
+console.log('cronjob started')
+cron.schedule("0 */4 * * *", async () => {
+  try {
+    const fourHoursAgo = new Date();
+    fourHoursAgo.setHours(fourHoursAgo.getHours() - 4);
+
+    const result = await OTPModel.deleteMany({
+      createdAt: { $lt: fourHoursAgo },
+    });
+    console.log(`Deleted ${result.deletedCount} OTPs older than 4 hours`);
+  } catch (err) {
+    console.log("Error deleting OTPs:", err.message);
+  }
+});
