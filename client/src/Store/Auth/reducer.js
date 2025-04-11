bab4f118b42cf5619071e5d9d6ff59daff400749
Söùrãv <souravpramanick1997@gmail.com>
2025-04-12 02:04:29 +0530

    last_name and dob added in user

--- client/src/Store/Auth/reducer.js
+++ client/src/Store/Auth/reducer.js
@@ -1,21 +1,27 @@
-import {ADD_EMAIL,LOGIN,LOGOUT} from '../actionTypes'
+import { ADD_EMAIL, LOGIN, LOGOUT } from "../actionTypes";
 const defaultUser = {
   token: null,
   name: "",
+  last_name: "",
+  dob: "",
   email: "",
-  gender:"",
-  mobile:""
+  gender: "",
+  mobile: "",
 };
-export default function authReducer(state = JSON.parse(localStorage.getItem("auth")) ||defaultUser, { type, payload }) {
+export default function authReducer(
+  state = JSON.parse(localStorage.getItem("auth")) || defaultUser,
+  { type, payload }
+) {
   switch (type) {
-    case ADD_EMAIL: return {...state,email:payload}
-    case LOGIN : {
-      localStorage.setItem("auth",JSON.stringify(payload))
-      return payload
+    case ADD_EMAIL:
+      return { ...state, email: payload };
+    case LOGIN: {
+      localStorage.setItem("auth", JSON.stringify(payload));
+      return payload;
     }
-    case LOGOUT:{
-      localStorage.removeItem('auth')
-      return defaultUser
+    case LOGOUT: {
+      localStorage.removeItem("auth");
+      return defaultUser;
     }
     default:
       return state;
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
     name:{type:String,required:true},
+    last_name:{type:String,default:""},
+    dob:{type:String,default:""},
     email:{type:String,required:true,unique:true},
     mobile:{type:Number,required:true,unique:true},
     gender:{type:String,required:true,enum:['male','female','other']}
